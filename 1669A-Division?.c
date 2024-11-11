5b1869de61c253a2fcd546de5758a0359d12b7a6
Azmain-999 <165258559+Azmain-999@users.noreply.github.com>
2024-11-12 05:27:17 +0600

    Create 1669A-Division?.c

---
+++ 1669A-Division?.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int rating,t;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++)
+    {
+        scanf("%d",&rating);
+        if(1900<=rating)
+        {
+            printf("Division 1\n");
+        }
+        else if(1600<=rating && rating<=1899)
+        {
+            printf("Division 2\n");
+        }
+        else if(1400<=rating && rating<=1599)
+        {
+            printf("Division 3\n");
+        }
+        else if(rating<=1399)
+        {
+            printf("Division 4\n");
+        }
+    }
+    return 0;
+}
