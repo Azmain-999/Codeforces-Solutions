5b20112e3a736b6abcfd0c46b758f5ee1d93c7ee
Azmain-999 <165258559+Azmain-999@users.noreply.github.com>
2024-11-12 05:50:16 +0600

    Create 977A-Wrong Subtraction.c

---
+++ 977A-Wrong Subtraction.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,k;
+    scanf("%d %d",&n,&k);
+   
+    while(k-->0)
+    {
+        if(n%10!=0)
+        {
+            n--;
+        }
+        else
+        {
+            n=n/10;
+        }
+    }
+
+    printf("%d\n",n);
+    return 0;
+
+
+}
