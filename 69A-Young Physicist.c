1a7439a9cedc6a779a1ae98f42b713480166aaeb
Azmain-999 <165258559+Azmain-999@users.noreply.github.com>
2024-11-12 05:46:16 +0600

    Create 69A-Young Physicist.c

---
+++ 69A-Young Physicist.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int n,x,y,z,sumx=0,sumy=0,sumz=0;
+
+scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+
+        scanf("%d %d %d",&x,&y,&z);
+        sumx=sumx+x;
+        sumy=sumy+y;
+        sumz=sumz+z;
+    }
+
+        if(sumx==0 && sumy==0 && sumz==0)
+        {
+                printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+
+
+    return 0;
+}
