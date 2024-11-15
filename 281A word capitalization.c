d6c7d2c8b0c5582c95f752d9e00defd5c1f827d2
Azmain Fayek <165258559+Azmain-999@users.noreply.github.com>
2024-11-15 14:10:07 +0600

    Update 281A word capitalization.c

--- 281A word capitalization.c
+++ 281A word capitalization.c
@@ -6,11 +6,6 @@ int main()
     char s[1000];
     scanf("%s",s);
     s[0]=towupper(s[0]);
-    /*if(s[0]>=97 && s[0]<=122)
-    {
-      k=s[0]-32;
-      s[0]=char k;
-    }*/
     printf("%s",s);
     return 0;
 }
